a009faed3eadc81542b5cb9025e7a25478d58fd7
leostar <hi@leostar.top>
2024-08-14 11:58:37 +0800

    test: update test config

--- jest.config.ts
+++ jest.config.ts
@@ -3,7 +3,7 @@ import type { Config } from 'jest';
 const config: Config = {
   verbose: true,
   moduleFileExtensions: ['js', 'json', 'ts'],
-  rootDir: 'test',
+  rootDir: '.',
   testRegex: '.*\\.spec\\.ts$',
   transform: {
     '^.+\\.(t|j)s$': 'ts-jest',
